f70fd3536c4f125f3d4b47e1f470a89156e41ef1
Valeriy2912 <37571454+Valeriy2912@users.noreply.github.com>
2018-05-18 17:54:48 +0300

    Использую команду include в php, чтобы подключить общее горизонтальное меню

---
+++ Главная.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<?php
+include("menu.php")
+?>
+</head>
+<body>
+
+<h5><p>Содержание:</p></h5>
+<ol id="list">
+<li> <a href="./Галерея.html#Гиперновая">Гиперновая</a>
+<li> <a href="./Галерея.html#Сверхновая">Сверхновая</a>
+<li> <a href="./Галерея.html#Голубые">Голубые</a>
+<li> <a href="./Галерея.html#SS 433">SS 433</a>
+<li> <a href="./Галерея.html#Нейтронная">Нейтронная</a>
+<li> <a href="./Галерея.html#Двойная">Двойная</a>
+<li> <a href="./Галерея.html#Тесные">Тесные</a>
+<li> <a href="./Галерея.html#Бурый">Бурый карлик</a>
+<li> <a href="./Галерея.html#Белый">Белый карлик</a>
+<li> <a href="./Галерея.html#Красный">Красный Гигант</a>
+<li> <a href="./Галерея.html#Райе">Звёзды Во́льфа — Райе́ </a>
+<li> <a href="./Галерея.html#Т">Звёзды ти́па T Тельца́</a>
+
+</body>
+</html>
---
+++ Оставить отзыв.php
@@ -0,0 +1,24 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<?php
+include("menu.php")
+?>
+</head>
+<body>
+<form action='contactus.php' method="post">
+<p>Вы можете оставить отзыв о выставке, заполнив форму:</p> <p>Вам понравилась выставка?</p>
+<p><input type="radio" name="areyou"/> Да</p>
+<p><input type="radio" name="areyou"/> Нет</p>
+<p> Комментарий </p>
+<p><textarea rows="10" cols="40"></textarea></p>
+
+<p><input type="reset"></p>
+<p><input type="submit"></p>
+  
+<input type="hidden" value="klimmail.ru@mail.ru"/>
+
+</form>
+
+</body>
+</html>
